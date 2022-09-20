bf8799cb8e3b4c89770e3c7bc1420834f3fdde1f
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-09-20 14:20:15 -0600

    all task 1 complete

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//task 1.0
+const mainEl = document.querySelector('main')
+//task 1.1
+mainEl.style.backgroundColor = 'var(--main-bg)'
+//task 1.2
+mainEl.innerHTML = '<h1>SEI Rocks!</h1>'
+//task 1.3
+mainEl.setAttribute('class', 'flex-ctr')
+
